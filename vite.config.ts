77d0e55ffd470b924b44eb1981620942d4309ba3
Daisho Komiyama <daishokomiyama@gmail.com>
2022-09-29 00:56:43 -0400

    Add Dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: { port: 5050 }
 };
 
 export default config;
