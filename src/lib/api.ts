89671f599633fe80c4390b6f761c5ed155708e8b
kdaisho <daishokomiyama@gmail.com>
2022-10-05 18:16:08 -0400

    Add ci script

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,11 @@
 import type { ApiParams } from 'src/types';
 
-const host = process.env.NODE_ENV === 'development' ? '0.0.0.0' : 'deno-net';
+const host = process.env.NODE_ENV === 'development' ? '0.0.0.0' : '35.183.5.18';
 const port = 3009;
 
 const baseUrl = `http://${host}:${port}`;
+
+console.log({ baseUrl });
 
 export default function api({ method, resource = '', data }: ApiParams) {
 	return fetch(`${baseUrl}/${resource}`, {
