07763fc0f6b607eda680dfae7677f9e720054b3e
kdaisho <daishokomiyama@gmail.com>
2022-10-19 09:45:10 -0400

    Add submodule proto

--- src/routes/(main)/connection.ts
+++ src/routes/(main)/connection.ts
@@ -2,7 +2,7 @@ import type { HelloPackage } from 'src/hello';
 import grpc from '@grpc/grpc-js';
 import protoLoader from '@grpc/proto-loader';
 
-const packageDef = protoLoader.loadSync('hello.proto', {});
+const packageDef = protoLoader.loadSync('proto/hello.proto', {});
 const grpcObject = grpc.loadPackageDefinition(packageDef);
 const helloPackage = grpcObject.helloPackage as HelloPackage;
 
