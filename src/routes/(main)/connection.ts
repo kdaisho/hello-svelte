8a963d6be75edca8ac46b79ecadd4ed283619dcf
kdaisho <daishokomiyama@gmail.com>
2022-10-21 10:36:55 -0400

    Deploy json-rpc branch to sandbox

--- src/routes/(main)/connection.ts
+++ src/routes/(main)/connection.ts
@@ -1,8 +1,13 @@
 import { JSONRPCClient, type JSONRPCResponse } from 'json-rpc-2.0';
 
+console.log({ environment: process.env.NODE_ENV });
+
+const host =
+	process.env.NODE_ENV === 'development' ? '0.0.0.0' : 'hello-project-service-deno.local';
+
 const client = new JSONRPCClient(async (request) => {
 	try {
-		const response = await fetch('http://0.0.0.0:3009', {
+		const response = await fetch(`http://${host}:3009`, {
 			method: 'POST',
 			headers: {
 				'content-type': 'application/json'
